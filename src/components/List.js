269455c7ed5c194b5b63ad1ba3d8fddecde5e5d6
Jin Young Choi <jinyoungchoi@alumni.emory.edu>
2021-06-11 16:26:11 -0700

    initialised repo directory structure & redundant files omitted

---
+++ src/components/List.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import todoItems from "../todoItems"
+
+function List() {
+    console.log(todoItems)
+    return (
+        <div className="list">
+            <h3>placeholder</h3>
+        </div>
+    )
+}
+
+export default List
---
+++ src/todoItems.js
@@ -0,0 +1,19 @@
+const todoItems = [
+    {
+        id: 0,
+        description: "daily walk",
+        checked: false
+    },
+    {
+        id: 1,
+        description: "workout",
+        checked: false
+    },
+    {
+        id: 2,
+        description: "journal",
+        checked: false
+    }
+]
+
+export default todoItems
